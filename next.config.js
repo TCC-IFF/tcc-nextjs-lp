81049f77788874f8a3724cd68e6c745722d4736a
caiodavid <caiodavid111@gmail.com>
2023-05-27 23:01:02 -0300

    chore: update next and next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.themoviedb.org',
+        port: '',
+        pathname: '/t/p/original/**'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig

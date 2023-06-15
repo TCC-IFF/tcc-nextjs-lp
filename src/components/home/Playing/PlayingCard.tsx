2d858cc60e4d8a10a29e5dedeb7219a3f94f680f
caiodavid <caiodavid111@gmail.com>
2023-06-15 09:18:05 -0300

    feat: use interative banner component

--- src/components/home/Playing/PlayingCard.tsx
+++ src/components/home/Playing/PlayingCard.tsx
@@ -1,5 +1,5 @@
+import { MovieInteractiveBanner } from '@/components/shared/MovieInterativeBanner'
 import { Movie } from '@/domains/types/tmdb'
-import Image from 'next/image'
 import { cinemaSessions } from './mocketSessions'
 
 export const PlayingCard = ({
@@ -24,23 +24,10 @@ export const PlayingCard = ({
 
   return (
     <div className="flex w-full mb-14">
-      <div className="relative flex rounded-md">
-        <Image
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt={movie.title}
-          width={200}
-          height={750}
-          className="rounded-md"
-        />
-        <div className="absolute hover:flex group transition-all items-end bottom-0 left-0 w-full h-full hover:bg-gradient-to-t from-slate-500 to-transparent">
-          <button
-            onClick={onClick}
-            className="w-full h-12 bg-rose-600 hidden group-hover:flex text-center text-white font-semibold items-center justify-center"
-          >
-            Ver mais
-          </button>
-        </div>
-      </div>
+      <MovieInteractiveBanner
+        movieBanner={movie.poster_path}
+        onClick={onClick}
+      />
       <div className="flex flex-col w-full ml-8 h-70">
         <div className="flex flex-col p-3 bg-rose-900 w-full rounded-lg">
           <h3 className="font-semibold text-slate-100">{movie.title}</h3>

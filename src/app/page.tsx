48e08c2490f27384a3c874f8ea377c5fdabeb203
caiodavid <caiodavid111@gmail.com>
2023-06-15 09:16:22 -0300

    feat: add upcoming section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import { Header, Navbar, Newsletter, Playing } from '@/components/home'
-import { NowPlayingResponse } from '@/domains/types/tmdb'
+import {
+  Header,
+  Navbar,
+  Newsletter,
+  Playing,
+  Upcoming
+} from '@/components/home'
+import { TmdbResponse } from '@/domains/types/tmdb'
 
-async function getData(): Promise<NowPlayingResponse> {
+async function getPlayingMoviesData(): Promise<TmdbResponse> {
   const url =
     'https://api.themoviedb.org/3/movie/now_playing?language=pt-br&page=1&region=BR'
   const options = {
@@ -20,13 +26,34 @@ async function getData(): Promise<NowPlayingResponse> {
   return res.json()
 }
 
+async function getUpcomingMoviesData(): Promise<TmdbResponse> {
+  const url =
+    'https://api.themoviedb.org/3/movie/upcoming?language=pt-br&page=1&region=BR'
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+    }
+  }
+  const res = await fetch(url, options)
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
 export default async function Home() {
-  const playingMoviesData = await getData()
+  const playingMoviesData = await getPlayingMoviesData()
+  const upcomingMoviesData = await getUpcomingMoviesData()
   return (
     <div className=" flex-col inline-block w-full h-screen bg-slate-900">
       <Navbar />
       <Header />
       <Playing playingMovies={playingMoviesData.results} />
+      <Upcoming upcomingMovies={upcomingMoviesData.results} />
       <Newsletter />
     </div>
   )

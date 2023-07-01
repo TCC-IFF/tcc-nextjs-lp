ff99188181f4001ca76c77ee7c8f470208b7d2f2
caiodavid <caiodavid111@gmail.com>
2023-07-01 09:31:30 -0300

    feat: add popular movies list call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,9 +47,31 @@ async function getUpcomingMoviesData(): Promise<TmdbResponse> {
   return res.json()
 }
 
+async function getPopularMoviesData(): Promise<TmdbResponse> {
+  const url =
+    'https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=12'
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+    }
+  }
+  const res = await fetch(url, options)
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
 export default async function Home() {
   const playingMoviesData = await getPlayingMoviesData()
   const upcomingMoviesData = await getUpcomingMoviesData()
+  const popularMoviesData = await getPopularMoviesData()
+
+  console.log(popularMoviesData)
   return (
     <div className=" flex-col inline-block w-full h-screen bg-slate-900">
       <Navbar />
